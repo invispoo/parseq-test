a292ccbd57bc0e902d48821f347dde2f94db5b5a
Olga Kashapova <invispo16@gmail.com>
2024-08-03 09:02:09 +0300

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/mutations': 'https://testapi2.parseq.pro',
-    },
-    cors: false,
-  },
 });
