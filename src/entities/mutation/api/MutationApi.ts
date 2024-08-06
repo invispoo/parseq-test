62a95ba2f35f1c6004e296ff543e2107e4991363
Olga Kashapova <invispo16@gmail.com>
2024-08-06 09:39:32 +0300

    deleted axios-retry

--- src/entities/mutation/api/MutationApi.ts
+++ src/entities/mutation/api/MutationApi.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { Mutation } from '../model/Mutation.ts';
-import axiosRetry, { isNetworkOrIdempotentRequestError } from 'axios-retry';
 
 export type MutationApiResponse = {
   page: {
@@ -11,17 +10,6 @@ export type MutationApiResponse = {
   resourcesTotalNumber: number;
 };
 
-// Код 'ECONNABORTED' появится в случае отмены запроса после истечения установленного в запросе таймаута (10 секунд),
-// поэтому в интерцепторе из библиотеки axios-retry необходимо прописать данный код как условие перезапуска запроса
-axiosRetry(axios, {
-  retries: 5,
-  retryDelay: () => 1000,
-  shouldResetTimeout: true,
-  retryCondition: (error: AxiosError) => {
-    return isNetworkOrIdempotentRequestError(error) || error.code === 'ECONNABORTED';
-  },
-});
-
 export class MutationApi {
   static async fetchAllMutations(pageNumber: number, pageSize: number): Promise<MutationApiResponse> {
     return (
@@ -30,7 +18,6 @@ export class MutationApi {
           pageZeroBasedNumber: pageNumber,
           pageSize: pageSize,
         },
-        timeout: 10000,
       })
     ).data;
   }
